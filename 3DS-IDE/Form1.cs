a7c4f0a6cace1a6577894a78d535398434875396
Atlantic <atlanticbit@icloud.com>
2015-03-11 15:48:05 +0100

    form finished and added icons

--- 3DS-IDE/Form1.cs
+++ 3DS-IDE/Form1.cs
@@ -16,5 +16,38 @@ namespace _3DS_IDE
         {
             InitializeComponent();
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Text = "Integrity check";
+            timer1.Stop();
+            timer2.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            this.Text = "Integrity check.";
+            timer2.Stop();
+            timer3.Start();
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            this.Text = "Integrity check..";
+            timer3.Stop();
+            timer4.Start();
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            this.Text = "Integrity check...";
+            timer4.Stop();
+            timer1.Start();
+        }
     }
 }
